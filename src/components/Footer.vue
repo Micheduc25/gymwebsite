ebed58e05e1d9b627594a3676d0b410cb5c2e46b
Tangu Achilis <tanguachilis@gmail.com>
2020-08-09 15:45:18 +0100

    added the contact on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -31,15 +31,15 @@
             </span>
           </div>
           <div class="flex flex-col">
-            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Footer header 3</span>
+            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Contact</span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">greatyoungminds237@gmail.com</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500 flex justify-center sm:justify-start"><img src="/images/icons/WhatsApp.svg" alt="" class="h-8 mr-1">651408846</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500 flex justify-center sm:justify-start"><img src="/images/icons/facebook.svg" alt="" class="h-8 mr-1">G.Y.M</a>
             </span>
           </div>
         </div>
