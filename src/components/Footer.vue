8573b8ee612ed047c2b0042e6747bbed95731e6c
Mich25Educ <ndjockjunior@gmail.com>
2020-08-09 01:44:41 +0100

    modified footer, added fields

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,40 +7,60 @@
           class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col sm:flex-row flex-wrap justify-between"
         >
           <div class="flex flex-col">
-            <span class="font-bold text-gray-700 uppercase mb-2">Footer header 1</span>
+            <span class="font-bold text-gray-700 uppercase mb-2">IT Services</span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Web and Mobile App Development</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">IT System Security</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
-            </span>
-          </div>
-          <div class="flex flex-col">
-            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Footer header 2</span>
-            <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Big Data and Neural Networks</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
-            </span>
-            <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Network Installation</a>
             </span>
           </div>
           <div class="flex flex-col">
-            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Footer header 3</span>
+            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Electronics Services</span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">House and Company wiring</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Smart Devices and Buildings</a>
             </span>
             <span class="my-2">
-              <a href="#" class="text-blue-700 text-md hover:text-blue-500">link 1</a>
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Electronics Creation Maintainance</a>
             </span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Robotics and AI</a>
+            </span>
+          </div>
+          <div class="flex flex-col">
+            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Architectural Design</span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">CAD of Buildings</a>
+            </span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Architectural Plans and Prototypes</a>
+            </span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Physical Implementation of Structures</a>
+            </span>
+          </div>
+
+          <div class="flex flex-col">
+            <span class="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">Mechanical Services</span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Design and Creation of Mechanical Systems</a>
+            </span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Automobile Maintenance and Repair </a>
+            </span>
+            <span class="my-2">
+              <a href="#" class="text-blue-700 text-md hover:text-blue-500">Technical Advice</a>
+            </span>
+
           </div>
         </div>
       </div>
